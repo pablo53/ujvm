e12a61e646f7fc87dc82fcf2b3f954ac7374e7be
Paweł Ryszawa <pablo53@poczta.onet.eu>
2020-07-12 02:34:26 +0200

    listing const pool entries

--- java.cpp
+++ java.cpp
@@ -10,13 +10,85 @@
 #include "defs/types.h"
 
 
-using namespace std;
-
 
 void print_utf8(const u8 * buf, u16 buflen)
 {
   for (u16 i = 0; i < buflen; i++)
-    printf("%c", buf[i]);
+    std::cout << buf[i];
+}
+
+void desc_class(ClassFile &klz)
+{
+  std::cout << "Constant Pool:" << std::endl;
+  for (u16 i = 0; i < klz.const_pool_cnt; i++)
+  {
+    ConstPoolEntry *cpe = &klz.const_pool[i - 1];
+    std::cout << "  [" << std::dec << i << "] ";
+    switch (cpe->tag)
+    {
+    case CONST_UTF8:
+      {
+        CPUtf8Info * utf8_info = (CPUtf8Info *)cpe->info;
+        std::cout << "UTF8       : ";
+        print_utf8(utf8_info->bytes, utf8_info->length);
+      }
+      break;
+    case CONST_CLASS:
+      {
+        CPClassInfo * class_info = (CPClassInfo *)cpe->info;
+        std::cout << "CLASS      : ";
+        std::cout << std::dec << class_info->name_idx;
+      }
+      break;
+    case CONST_STRING:
+      {
+        CPStringInfo * string_info = (CPStringInfo *)cpe->info;
+        std::cout << "STRING     : ";
+        std::cout << std::dec << string_info->str_idx;
+      }
+      break;
+    case CONST_FIELD_REF:
+      {
+        CPFieldRefInfo * fldref_info = (CPFieldRefInfo *)cpe->info;
+        std::cout << "FIELD REF  : ";
+        std::cout << std::dec << fldref_info->class_idx << ", " << fldref_info->name_typ_idx;
+      }
+      break;
+    case CONST_METHOD_REF:
+      {
+        CPMethodRefInfo * methref_info = (CPMethodRefInfo *)cpe->info;
+        std::cout << "METHOD REF : ";
+        std::cout << std::dec << methref_info->class_idx << ", " << methref_info->name_typ_idx;
+      }
+      break;
+    case CONST_NAME_AND_TYPE:
+      {
+        CPNameTypeInfo * nametyp_info = (CPNameTypeInfo *)cpe->info;
+        std::cout << "NAME & TYPE: ";
+        std::cout << std::dec << nametyp_info->name_idx << ", " << nametyp_info->desc_idx;
+      }
+      break;
+    case CONST_METHOD_HANDLE:
+      {
+        CPMethodHdlInfo * methhdl_info = (CPMethodHdlInfo *)cpe->info;
+        std::cout << "METHOD HDLR: ";
+        std::cout << std::hex << "0x" << (u16)methhdl_info->ref_kind << ", " << std::dec << methhdl_info->ref_idx;
+      }
+      break;
+    case CONST_INVOKE_DYNAMIC:
+      {
+        CPInvokeDynamicInfo * invdyn_info = (CPInvokeDynamicInfo *)cpe->info;
+        std::cout << "INVOKE DYN : ";
+        std::cout << std::hex << invdyn_info->boot_meth_attr_idx << ", " << std::dec << invdyn_info->name_typ_idx;
+      }
+      break;
+    // TODO
+    default:
+      std::cout << "0x" << std::hex << (int)cpe->tag << ":";
+      break;
+    }
+    std::cout << std::endl;
+  }
 }
 
 u8 * load_file(const char * fname, long &size)
@@ -32,7 +104,7 @@ u8 * load_file(const char * fname, long &size)
     cur += (cnt = fread(&buf[cur], 1, (size_t)size - cur, f));
   res = fclose(f);
 
-  cout << "File '" << fname << "' length: " << size << endl;
+  std::cout << "File '" << fname << "' length: " << size << std::endl;
   return buf;
 }
 
@@ -51,10 +123,11 @@ int main(int args, char *argv[])
 {
   if (args <= 1)
   {
-    printf("%s", "uJVM (Micro Java Virtual Machine)\n");
+    std::cout << "uJVM (Micro Java Virtual Machine)" << std::endl;
     return -1;
   }
   ClassFile klz = load_class(argv[1]);
+  desc_class(klz);
   
   return 0;
 }
