71448bfbe3a53efb00d5076191c2388185045c52
Paweł Ryszawa <pablo53@poczta.onet.eu>
2020-08-19 01:52:03 +0200

    Buffered JavaUtf8 for algorithms.

---
+++ alg/buf.cpp
@@ -0,0 +1,38 @@
+#ifndef _ALG_BUF_CPP
+#define _ALG_BUF_CPP
+
+
+#include "buf.h"
+
+#include "../defs/types.h"
+#include "../class/utf8.h"
+
+
+Utf8Buffer::Utf8Buffer(const JavaUtf8 * utf8)
+{
+  this->utf8 = utf8;
+  this->curs = 0;
+}
+
+u16 Utf8Buffer::chars_left() const
+{
+  return utf8->get_length() - curs;
+}
+
+jchar Utf8Buffer::peek(u16 deepness) const
+{
+  return (chars_left() > deepness) ? utf8->get_jchar(curs + deepness) : 0;
+}
+
+jchar Utf8Buffer::next()
+{
+  return chars_left() ? utf8->get_jchar(curs++) : 0;
+}
+
+u16 Utf8Buffer::get_length() const
+{
+  return utf8->get_length();
+}
+
+
+#endif
---
+++ alg/buf.h
@@ -0,0 +1,26 @@
+#ifndef _ALG_BUF_H
+#define _ALG_BUF_H
+
+
+#include "../defs/types.h"
+#include "../class/utf8.h"
+
+class Utf8Buffer
+{
+  public:
+  const JavaUtf8 * utf8; /* no ownership */
+  u16 curs; /* cursor */
+
+  Utf8Buffer(const JavaUtf8 * utf8); /* no ownership; utf8 should not be changed externally! */
+  Utf8Buffer() = delete;
+  Utf8Buffer(const Utf8Buffer &) = delete;
+  Utf8Buffer(Utf8Buffer &&) = delete;
+
+  u16 chars_left() const; /* how many chars left there are to the end of buffer */
+  jchar peek(u16 = 0) const; /* default deepness = 0 means the next jchar that would normally be read; returns 0, if cannot peek that deep (check with chars_left method first!) */
+  jchar next(); /* reads the next jchar from buffer; returns 0, if there are no more characters left (check with chars_left method first!) */
+  u16 get_length() const;
+};
+
+
+#endif
